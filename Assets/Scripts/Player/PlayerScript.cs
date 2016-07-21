a993f75dcccda1fcaf6589910e3903a6545425bd
Blackop778 <nefalt@att.net>
2016-07-21 14:26:33 -0500

    Started the project
    Though we should probably start with the opening sequence, not the
    Overworld

---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(Vector3.up);
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(Vector3.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(Vector3.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(Vector3.left);
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/Follow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+    public Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = target.position.x - transform.position.x;
+        float y = target.position.y - transform.position.y;
+        transform.Translate(new Vector3(x, y, 0f));
+    }
+}
