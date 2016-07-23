d00a20c86f37ff3e461d7d649319e544f7585ce1
Blackop778 <nefalt@att.net>
2016-07-23 14:08:20 -0500

    Animations pretty much work
    I may have been making it way too complicated

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -4,31 +4,39 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
 
     private Animator animator;
+    private int movementStateHash = Animator.StringToHash("MovementState");
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
+        animator.SetFloat(movementStateHash, .3f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.up);
-            //animator.SetBool("Walk up", true);
+            transform.Translate(Vector3.up * 5 * Time.deltaTime);
+            animator.SetFloat(movementStateHash, 1.1f);
         }
-        else if(Input.GetKeyDown(KeyCode.S))
+        else if(Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.down);
-            //animator.SetBool("Walk up", false);
+            transform.Translate(Vector3.down * 5 * Time.deltaTime);
+            animator.SetFloat(movementStateHash, 1f);
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right);
+            transform.Translate(Vector3.right * 5 * Time.deltaTime);
+            animator.SetFloat(movementStateHash, 1.2f);
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left);
+            transform.Translate(Vector3.left * 5 * Time.deltaTime);
+            animator.SetFloat(movementStateHash, 1.3f);
+        }
+        else if(animator.GetFloat(movementStateHash) >= 1)
+        {
+            animator.SetFloat(movementStateHash, animator.GetFloat(movementStateHash) - 1f);
         }
     }
 }
