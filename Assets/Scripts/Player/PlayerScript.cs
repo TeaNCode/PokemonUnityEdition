0e8215cb3ac5d151d0a783efca2ae656872b88bc
Blackop778 <nefalt@att.net>
2016-07-24 22:57:46 -0500

    Integrated Pokemon's movement system
    TODO: fix slight variations in distance moved
    TODO: investigate sliding around with quick button presses

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -5,38 +5,60 @@ public class PlayerScript : MonoBehaviour {
 
     private Animator animator;
     private int movementStateHash = Animator.StringToHash("MovementState");
+    private float animationTimer;
+    private int counter;
+    private Vector3 direction;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
         animator.SetFloat(movementStateHash, .3f);
+        animationTimer = 0;
+        counter = 0;
+        direction = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.W))
+        animationTimer -= Time.deltaTime * 4;
+        if(animationTimer > 0)
         {
-            transform.Translate(Vector3.up * 5 * Time.deltaTime);
-            animator.SetFloat(movementStateHash, 1.1f);
+            transform.Translate(direction * 4 * Time.deltaTime);
         }
-        else if(Input.GetKey(KeyCode.S))
+        if (animationTimer <= 0f)
         {
-            transform.Translate(Vector3.down * 5 * Time.deltaTime);
-            animator.SetFloat(movementStateHash, 1f);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * 5 * Time.deltaTime);
-            animator.SetFloat(movementStateHash, 1.2f);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * 5 * Time.deltaTime);
-            animator.SetFloat(movementStateHash, 1.3f);
-        }
-        else if(animator.GetFloat(movementStateHash) >= 1)
-        {
-            animator.SetFloat(movementStateHash, animator.GetFloat(movementStateHash) - 1f);
+            if (Input.GetKey(KeyCode.W))
+            {
+                direction = Vector3.up;
+                transform.Translate(direction * 4 * Time.deltaTime);
+                animator.SetFloat(movementStateHash, 1.1f);
+                animationTimer = 1f;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                direction = Vector3.down;
+                transform.Translate(direction * 4 * Time.deltaTime);
+                animator.SetFloat(movementStateHash, 1f);
+                animationTimer = 1f;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                direction = Vector3.right;
+                transform.Translate(direction * 4 * Time.deltaTime);
+                animator.SetFloat(movementStateHash, 1.2f);
+                animationTimer = 1f;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                direction = Vector3.left;
+                transform.Translate(direction * 4 * Time.deltaTime);
+                animator.SetFloat(movementStateHash, 1.3f);
+                animationTimer = 1f;
+            }
+            else if (animator.GetFloat(movementStateHash) >= 1)
+            {
+                animator.SetFloat(movementStateHash, animator.GetFloat(movementStateHash) - 1f);
+            }
         }
     }
 }
