0313c1b584c1f497c2337771e9ddb9d79240aef2
Blackop778 <nefalt@att.net>
2016-07-26 22:51:44 -0500

    Added collision
    Except it doesn't work from the top and looks really bad

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -5,60 +5,83 @@ public class PlayerScript : MonoBehaviour {
 
     private Animator animator;
     private int movementStateHash = Animator.StringToHash("MovementState");
-    private float animationTimer;
-    private int counter;
+    private float movementTimer;
     private Vector3 direction;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
         animator.SetFloat(movementStateHash, .3f);
-        animationTimer = 0;
-        counter = 0;
+        movementTimer = 0;
         direction = Vector3.zero;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        animationTimer -= Time.deltaTime * 4;
-        if(animationTimer > 0)
+        movementTimer -= Time.deltaTime * 4;
+        if(movementTimer > 0)
         {
             transform.Translate(direction * 4 * Time.deltaTime);
         }
-        if (animationTimer <= 0f)
+        else if (movementTimer <= 0f)
         {
+            roundLocation();
             if (Input.GetKey(KeyCode.W))
             {
+                roundLocation();
                 direction = Vector3.up;
                 transform.Translate(direction * 4 * Time.deltaTime);
                 animator.SetFloat(movementStateHash, 1.1f);
-                animationTimer = 1f;
+                movementTimer = 1f;
             }
             else if (Input.GetKey(KeyCode.S))
             {
+                roundLocation();
                 direction = Vector3.down;
                 transform.Translate(direction * 4 * Time.deltaTime);
                 animator.SetFloat(movementStateHash, 1f);
-                animationTimer = 1f;
+                movementTimer = 1f;
             }
             else if (Input.GetKey(KeyCode.D))
             {
+               roundLocation();
                 direction = Vector3.right;
                 transform.Translate(direction * 4 * Time.deltaTime);
                 animator.SetFloat(movementStateHash, 1.2f);
-                animationTimer = 1f;
+                movementTimer = 1f;
             }
             else if (Input.GetKey(KeyCode.A))
             {
+                roundLocation();
                 direction = Vector3.left;
                 transform.Translate(direction * 4 * Time.deltaTime);
                 animator.SetFloat(movementStateHash, 1.3f);
-                animationTimer = 1f;
+                movementTimer = 1f;
             }
-            else if (animator.GetFloat(movementStateHash) >= 1)
+            else
             {
-                animator.SetFloat(movementStateHash, animator.GetFloat(movementStateHash) - 1f);
+                roundLocation();
+                if (animator.GetFloat(movementStateHash) >= 1)
+                {
+                    animator.SetFloat(movementStateHash, animator.GetFloat(movementStateHash) - 1f);
+                }
             }
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        roundLocation();
+    }
+
+    private void roundLocation()
+    {
+        Vector3 location = transform.position;
+        int xGoal = Mathf.RoundToInt(location.x);
+        int yGoal = Mathf.RoundToInt(location.y);
+        float xDistance = xGoal - location.x;
+        float yDistance = yGoal - location.y;
+        transform.Translate(new Vector3(xDistance, yDistance, 0));
     }
 }
