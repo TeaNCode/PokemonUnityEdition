82c63ce63adec221db8727a62a63008e4303997f
Blackop778 <nefalt@att.net>
2016-08-14 14:58:31 -0500

    Added some variables

--- Assets/Scripts/Pokemon/BattlePokemon.cs
+++ Assets/Scripts/Pokemon/BattlePokemon.cs
@@ -4,4 +4,20 @@ using System.Collections;
 public class BattlePokemon
 {
     public Pokemon pokemon;
+    public sbyte hpStages;
+    public float hpMultiplier;
+    public sbyte attackStages;
+    public float attackMultiplier;
+    public sbyte defenseStages;
+    public float defenseMultiplier;
+    public sbyte specialAttackStages;
+    public float specialAttackMultiplier;
+    public sbyte specialDefenseStages;
+    public float specialDefenenseMultiplier;
+    public sbyte speedStages;
+    public float speedMultiplier;
+    public sbyte evasionStages;
+    public float evasionMultiplier;
+    public sbyte accuracyStages;
+    public sbyte accuracyMultiplier;
 }
